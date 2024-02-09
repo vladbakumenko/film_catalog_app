2db8c6939cd89f7918d94d8bfec88b4ac5cd68a7
vlad bakumenko <klaid-slaid@yandex.ru>
2024-02-10 01:34:24 +0400

    Initial commit

---
+++ src/main/java/ru/vladbakumenko/film_catalog_app/FilmCatalogAppApplication.java
@@ -0,0 +1,13 @@
+package ru.vladbakumenko.film_catalog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmCatalogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmCatalogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vladbakumenko/film_catalog_app/FilmCatalogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladbakumenko.film_catalog_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmCatalogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
